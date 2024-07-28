a30f92a37c08644d2cb8ae331f71f7e7c3f2e169
Pedro Martin <pedromartinf07@gmail.com>
2024-07-27 20:33:28 -0600

    update script

--- Actividad2/script.sh
+++ Actividad2/script.sh
@@ -6,13 +6,18 @@ url="https://api.github.com/users/"
 #read environment variable GITHUB_USER
 user=$GITHUB_USER
 
+if [ -z $user ]; then
+    echo "No se ha definido la variable de entorno GITHUB_USER"
+    exit 1
+fi
+
 #headers for petitions User Agent Mozilla
 header="User-Agent: Mozilla/5.0"
 
 #read username from user 
 response=$(curl -s -H "$header" $url$user)
 
-message="Hola $(echo $response | jq -r '.name') User ID: $(echo $response | jq -r '.id'). Cuenta creada el $(echo $response | jq -r '.created_at')"
+message="Hola $(echo $response | jq -r '.login') User ID: $(echo $response | jq -r '.id'). Cuenta creada el $(echo $response | jq -r '.created_at')"
 
 date=$(date +'%Y-%m-%d')
 
